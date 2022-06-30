f5f5c14c5fbfda4de7187b3e7b1112bde1c43bc9
Gitau01Frank <franciswainainag@gmail.com>
2022-06-30 09:00:18 -0700

    Task 6.

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int integerType;
+	float floatType;
+	double doubleType;
+	char charType;
+
+	printf("Size of int: %ld bytes\n", sizeof(integerType));
+	printf("Size of float: %ld bytes\n", sizeof(floatType));
+	printf("Size of doubleType: %ld bytes\n", sizeof(doubleType));
+	printf("Size of charType:%ld\n", sizeof(charType));
+	return (0);
+}
