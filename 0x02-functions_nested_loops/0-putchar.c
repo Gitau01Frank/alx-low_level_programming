12c70fddc934a93fd194c3a169cc873eea387d64
Gitau01Frank <franciswainainag@gmail.com>
2022-07-05 03:44:01 -0700

    Header file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * main - Prints putchar
  *
  * Return: Always 0 (Success)
  */
@@ -15,5 +15,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
